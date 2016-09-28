11e0852e050fd8d8186a71c499d4346ac331bb98
spazworm <spazworm@gmam>
2016-09-28 12:28:36 +0930

    re-arranging resque into different directory

--- recipes/resque-restart.rb
+++
@@ -1,7 +0,0 @@
-# originally from github.com/pedroaxl/opsworks-resque/blob/master/recipes/restart.rb
-node[:deploy].each do |application, deploy|
-  service "resque-#{application}" do
-    action [:stop, :start]
-    provider Chef::Provider::Service::Upstart
-  end
-end
